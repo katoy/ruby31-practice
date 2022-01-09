acc02e2626684262b663175e6c65dacebd7b52a3
katoy <youichikato@gmail.com>
2022-01-09 09:23:28 +0900

    hash の val 値の扱いを変更したものを追加

--- lib/convert_hash_syntax.rb
+++ lib/convert_hash_syntax.rb
@@ -5,3 +5,21 @@ def convert_hash_syntax(old_syntax)
     "#{Regexp.last_match(1)}: "
   end
 end
+
+def convert_hash_syntax2(old_syntax, nest = 0)
+  padding = '  ' * nest
+  str = "{\n"
+
+  eval(old_syntax).each do |k, v|
+    str +=
+      if v.instance_of?(Hash)
+        "#{padding}  #{k.to_sym}: #{convert_hash_syntax2(v.to_s, nest + 1)},\n"
+      else
+        "#{padding}  #{k.to_sym}: '#{v}',\n"
+      end
+  end
+
+  str += "#{padding}}"
+  str += "\n" if nest.zero?
+  str
+end
