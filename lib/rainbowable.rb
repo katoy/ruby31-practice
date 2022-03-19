28738da8e6afe5702b0c115c584a892836b5e57a
katoy <youichikato@gmail.com>
2022-03-19 15:06:47 +0900

    Rainbowable を, 色の決定処理, エスケープシーケンスで色をつける処理を分離して記述した

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -2,10 +2,23 @@
 
 # Rainbowable
 module Rainbowable
+  COLORS = [31, 32, 33, 34, 35, 36].freeze
+  NUM_COLORS = COLORS.size
+
   def rainbow
-    to_s.each_char.map.with_index do |char, count|
-      color = 31 + count % 6
-      "\e[#{color}m#{char}"
-    end.join + "\e[0m"
+    str = to_s
+    colors = (0...str.length).map { |idx| nth_color(idx) }
+    colors.zip(str.chars).map { |color, ch| colored_char(color, ch) }
+          .join + colored_char(0, '')
+  end
+
+  private
+
+  def colored_char(color, char)
+    "\e[#{color}m#{char}"
+  end
+
+  def nth_color(idx)
+    COLORS[idx % NUM_COLORS]
   end
 end
