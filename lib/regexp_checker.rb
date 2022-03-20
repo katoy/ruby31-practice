6d57a4200ff8567baf386dc2c72d105d6843f701
katoy <youichikato@gmail.com>
2022-03-20 12:56:38 +0900

    regexp_checker の test を追加

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,20 +1,37 @@
 # frozen_string_literal: true
 
-print 'Text?: '
-text = gets.chomp
+# 小mんどラインアプリ
+class RegexpChecker
+  def main
+    params = read_params
+    matches = find_matches(text: params[:text], regexp: params[:regexp])
+    puts show_matches(matches)
+  end
 
-begin
-  print 'Pattern?: '
-  pattern = gets.chomp
-  regexp = Regexp.new(pattern)
-rescue RegexpError => e
-  puts "Invalid pattern: #{e.message}"
-  retry
+  def read_params
+    print 'Text?: '
+    text = gets.chomp
+
+    begin
+      print 'Pattern?: '
+      pattern = gets.chomp
+      { text:, regexp: Regexp.new(pattern) }
+    rescue RegexpError => e
+      puts "Invalid pattern: #{e.message}"
+      retry
+    end
+  end
+
+  def find_matches(text: nil, regexp: nil)
+    text.scan(regexp)
+  end
+
+  def show_matches(matches)
+    return 'Nothing matched.' if matches.size.zero?
+
+    "Matched: #{matches.map { |x| "'#{x}'" }.join(', ')}"
+  end
 end
 
-matches = text.scan(regexp)
-if matches.size > 0
-  puts "Matched: #{matches.join(', ')}"
-else
-  puts 'Nothing matched.'
-end
+# コマンドラインで実行する
+RegexpChecker.new.main if __FILE__ == $PROGRAM_NAME
---
+++ test/regexp_checker_test.rb
@@ -0,0 +1,53 @@
+require_relative './test_helper'
+require_relative '../lib/regexp_checker'
+
+require 'stringio'
+
+# See
+# https://qiita.com/jnchito/items/23f623cdb4bed67f17f8
+# 正規表現チェッカープログラムのテストを自動化する
+#
+class RegexpCheckerTest < Minitest::Test
+  def test_main_when_matched
+    # ユーザーの入力値
+    io = StringIO.new(
+      "123-456-789\n" \
+      "[1-6+\n" \
+      "[1-6]+\n"
+    )
+
+    expected = [
+      'Text?: ',
+      'Pattern?: ',
+      "Invalid pattern: premature end of char-class: /[1-6+/\n",
+      'Pattern?: ',
+      "Matched: '123', '456'\n"
+    ].join
+
+    assert_output(expected) do
+      $stdin = io
+      RegexpChecker.new.main
+      $stdin = STDIN
+    end
+  end
+
+  def test_main_when_not_matched
+    # ユーザーの入力値
+    io = StringIO.new(
+      "abc-def-ghi\n" \
+      "[1-6]+\n"
+    )
+
+    expected = [
+      'Text?: ',
+      'Pattern?: ',
+      "Nothing matched.\n"
+    ].join
+
+    assert_output(expected) do
+      $stdin = io
+      RegexpChecker.new.main
+      $stdin = STDIN
+    end
+  end
+end
