bac336b76ef35ad0b068e608e59e48ea6fca8ed7
katoy <youichikato@gmail.com>
2022-03-20 17:13:38 +0900

    rainbowable を色セットを指定できるようにした

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -3,12 +3,20 @@
 # Rainbowable
 module Rainbowable
   COLORS = [31, 32, 33, 34, 35, 36].freeze
-  NUM_COLORS = COLORS.size
 
-  def rainbow
+  def rainbow(color_ary = nil)
+    rainbow_colors =
+      if color_ary.nil? || color_ary.size.zero?
+        COLORS
+      else
+        color_ary
+      end
+    colors_size = rainbow_colors.size
+
     str = to_s
-    colors = (0...str.length).map { |idx| nth_color(idx) }
-    colors.zip(str.chars).map { |color, ch| colored_char(color, ch) }
+    colors = (0...str.length).map { |idx| rainbow_colors[idx % colors_size] }
+    colors.zip(str.chars)
+          .map { |color, ch| colored_char(color, ch) }
           .join + colored_char(0, '')
   end
 
@@ -16,9 +24,5 @@ module Rainbowable
 
   def colored_char(color, char)
     "\e[#{color}m#{char}"
-  end
-
-  def nth_color(idx)
-    COLORS[idx % NUM_COLORS]
   end
 end
--- test/rainbowable_test.rb
+++ test/rainbowable_test.rb
@@ -16,4 +16,14 @@ class RainbowableTest < Minitest::Test
     expected = "\e[31m[\e[32m1\e[33m,\e[34m \e[35m2\e[36m,\e[31m \e[32m3\e[33m]\e[0m"
     assert_equal expected, [1, 2, 3].rainbow
   end
+
+  def test_rainbow_with_color_ary
+    expected = "\e[31ma\e[32mb\e[31mc\e[32md\e[31me\e[32mf\e[0m"
+    assert_equal expected, 'abcdef'.rainbow([31, 32])
+
+    expected = "\e[31ma\e[32mb\e[33mc\e[34md\e[35me\e[36mf\e[0m"
+    assert_equal expected, 'abcdef'.rainbow([])
+
+    assert_equal expected, 'abcdef'.rainbow(nil)
+  end
 end
