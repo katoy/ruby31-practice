b8d2b500c0043b28c01ed5863dac1731af8e46d5
katoy <youichikato@gmail.com>
2022-08-12 11:03:31 +0900

    log_formatter の rspec niduration 1000 の境界値テストを

--- spec/log_formatter_spec.rb
+++ spec/log_formatter_spec.rb
@@ -9,13 +9,10 @@ require 'json'
 
 RSpec.describe 'LogFormatter' do
   describe 'self.format_log' do
-    context 'form url' do
-      let(:log_data) do
-        uri = URI.parse('https://samples.jnito.com/access-log.json')
-        Net::HTTP.get(uri)
-      end
-
+    context 'log-data from url' do
+      let(:url) { 'https://samples.jnito.com/access-log.json' }
       let(:json_data) do
+        log_data = Net::HTTP.get(URI.parse(url)) 
         JSON.parse(log_data, symbolize_names: true)
       end
 
@@ -40,16 +37,16 @@ RSpec.describe 'LogFormatter' do
           },
           {
             "request_id": '2',
+            "path": '/products',
+            "status": 200,
+            "duration": 1023.8
+          },
+          {
+            "request_id": '3',
             "path": '/wp-login.php',
             "status": 404,
             "duration": 48.1,
             "error": 'Not found'
-          },
-          {
-            "request_id": '3',
-            "path": '/products',
-            "status": 200,
-            "duration": 1023.8
           },
           {
             "request_id": '4',
@@ -65,16 +62,40 @@ RSpec.describe 'LogFormatter' do
         text = LogFormatter.format_log(json_data)
         lines = text.lines(chomp: true)
         expect(lines[0]).to eq '[OK] request_id=1, path=/products/1'
-        expect(lines[1]).to eq '[ERROR] request_id=2, path=/wp-login.php, status=404, error=Not found'
-        expect(lines[2]).to eq '[WARN] request_id=3, path=/products, duration=1023.8'
+        expect(lines[1]).to eq '[WARN] request_id=2, path=/products, duration=1023.8'
+        expect(lines[2]).to eq '[ERROR] request_id=3, path=/wp-login.php, status=404, error=Not found'
         expect(lines[3]).to eq '[ERROR] request_id=4, path=/dangerous, status=500, error=Internal server error'
       end
     end
 
-    context 'enpty log' do
+    context 'duration' do
       let(:json_data) do
-        {}
+        [
+          {
+            "request_id": '1',
+            "path": '/products/1',
+            "status": 200,
+            "duration": 999.9
+          },
+          {
+            "request_id": '2',
+            "path": '/products/2',
+            "status": 200,
+            "duration": 1000
+          },
+        ]
       end
+
+      it 'duration < 1000 なら OK, そうでなければ WARN' do
+        text = LogFormatter.format_log(json_data)
+        lines = text.lines(chomp: true)
+        expect(lines[0]).to eq '[OK] request_id=1, path=/products/1'
+        expect(lines[1]).to eq '[WARN] request_id=2, path=/products/2, duration=1000'
+      end
+    end
+
+    context 'enpty log' do
+      let(:json_data) { {} }
 
       it do
         text = LogFormatter.format_log(json_data)
